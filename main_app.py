acab1994c655e019d684bbc61dea3c93640501b7
guijinSON <spthsrbwls123@yonsei.ac.kr>
2022-04-30 00:15:33 +0900

    .

--- main_app.py
+++ main_app.py
@@ -7,15 +7,19 @@ st.title('PRJ_빵댕이를 흔들 수 있습니까?')
 with open("text.md") as f:
     st.markdown(f.read())
 
-checkboxlv1 = st.checkbox('네, 알겠습니다!', key='lv1')
+checkboxlv1 = st.checkbox('너무 좋아요!', key='lv1')
 if checkboxlv1:
     st.image('im.jpeg')
 
-    with open("money.md") as f:
+    with open("info.md") as f:
         st.markdown(f.read())
-    
-    checkboxlv2 = st.checkbox('네, 알겠습니다!')
+    checkboxlv2 = st.checkbox('네, 초대할게요!')
     if checkboxlv2:
+        with open("money.md") as f:
+            st.markdown(f.read())
+
+        checkboxlv3 = st.checkbox('네, 알겠습니다!')
+        if checkboxlv3:
 
         with st.form(key='my_form'):
             name = st.text_input(label='이름을 입력하세요')
